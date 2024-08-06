36305cb4559e0ceb4585d0d0abfcb3e8806b3738
Rajib <fakhrirajib.work@gmail.com>
2024-08-06 19:23:51 +0700

    Soal 1 done!

--- java/Soal1.java
+++ java/Soal1.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Soal1 {
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Nama Film: ");
+        String namaFilm = scn.nextLine();
+        System.out.print("Harga Tiket: ");
+        int hargaTiket = scn.nextInt();
+        System.out.print("Jumlah Tiket: ");
+        int jumlahTiket = scn.nextInt();
+
+        System.out.println("");
+        System.out.println("Nama Film: " + namaFilm);
+        System.out.println("Harga Tiket: " + hargaTiket);
+        System.out.println("Jumlah Tiket " + jumlahTiket);
+
     }
 }
