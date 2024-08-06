17db0ad688a08371446d03820bdacd082f0a18e0
Rajib <fakhrirajib.work@gmail.com>
2024-08-06 20:22:56 +0700

    Soal 4 done!

---
+++ java/Soal4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Soal4 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String[] listFilm = new String[6];
+
+        System.out.println("Daftar Film Bioskop");
+        for(int i = 1; i <= 5; i++) {
+            System.out.print("Masukan nama film " + i +": ");
+            listFilm[i] = scn.nextLine();
+        }
+
+        System.out.println("");
+
+        System.out.println("Film yang ingin ditonton:");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i + "." + " " +listFilm[i]);
+        }
+    }
+}
