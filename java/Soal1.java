3b16666809e995018ad40b10a48b3653452aa2d8
Rajib <fakhrirajib.work@gmail.com>
2024-08-06 08:54:24 +0700

    test2

--- java/Soal1.java
+++ java/Soal1.java
@@ -1,5 +1,7 @@
 public class Soal1 {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
