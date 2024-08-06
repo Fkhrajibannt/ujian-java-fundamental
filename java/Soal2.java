0bee70cb4a48f1d85606d94cf132b64f1f635739
Rajib <fakhrirajib.work@gmail.com>
2024-08-06 19:36:49 +0700

    Soal 2 done!

---
+++ java/Soal2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Soal2 {
+    public static void  main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Masukan nama film: ");
+        String namaFilm = scn.nextLine();
+
+        System.out.println("");
+
+        System.out.println("Nama film dalam huruf besar: " + namaFilm.toUpperCase());
+    }
+}
